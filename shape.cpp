c2b517bd0a309e45dfaa965ab057b37b1210713b
Elyesse BENYELOUL <elyesse.benyeloul@etu.minesparis.psl.eu>
2025-01-14 14:43:20 +0100

    shape

--- shape.cpp
+++ shape.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>
+
 class Shape
 {
 private:
@@ -35,3 +38,15 @@ public:
     Rectangle(int u = 0, int v = 0, int l, int w) : length(l), width(w) { fig = Shape(u, v); };
     int area() { return length * width; };
 };
+
+int main()
+{
+    Circle c{10, 20, 4.5};
+    Rectangle r{20, 30, 30, 60};
+    std::vector<Shape *> shapes;
+    shapes.push_back(&c);
+    shapes.push_back(&r);
+    shapes[0]->move(4, 6);
+    shapes[1]->move(2, 8);
+    return 0;
+}
